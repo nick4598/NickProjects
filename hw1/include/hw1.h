39d649b8d051d5a8b4847911a2287d38ce54bbb7
nick4598 <nick4598@gmail.com>
2018-09-13 15:40:13 -0400

    Upload MIPS Assembler Disassembler
    MIPS Assembler Disassembler source code, header files, and test files.

---
+++ hw1/include/hw1.h
@@ -0,0 +1,11 @@
+#ifndef HW_H
+#define HW_H
+
+#include "const.h"
+#include "instruction.h"
+
+int isSameString(char *str1, char *str2);
+int validateBaseAddress(char * address);
+int flipEndianness(int numToFlip);
+
+#endif
